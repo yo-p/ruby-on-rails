795afe4e2812408f7cefeb4cb6e921decbce2313
yo-p <papapapipipi42@gmail.com>
2020-05-07 01:33:02 +0000

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   namespace :admin do
     resources :users
   end
   root to: 'tasks#index'
   resources :tasks
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
